485a03261cbf61825204d40797ae37ad49a89a0b
Jonas Ullmann <jonas.ullmann@josephinum.at>
2024-02-14 17:39:05 +0100

    nicht viel

---
+++ SEW02_07_06_Catchme/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SEW02_07_06_Catchme
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ SEW02_07_06_Catchme/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SEW02_07_06_Catchme
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            int maxLeft = Convert.ToInt32(Grid.ActualWidth - button1.Width);
+            int maxTop = Convert.ToInt32(Grid.ActualHeight - button1.Height);
+            Random rand = new Random();
+            button1.Margin = new Thickness(rand.Next(maxLeft), rand.Next(maxTop), 0, 0);
+        }
+    }
+}
